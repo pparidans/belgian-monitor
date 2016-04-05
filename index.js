5cb3606c4aae0e5a42dfec0714775f8a0c08d743
Pierre Paridans <pierre@paridans.org>
2016-04-05 13:08:45 +0200

    transform into a JSON API

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+import express from 'express'
+import morgan from 'morgan'
 import parser from './belgian-monitor-parser'
 
-const enterpriseNumber = process.argv[2] || 846419822
+let app = express()
+app.set('port', process.env.PORT || 3000)
+app.use(morgan('combined'))
 
-parser.searchDocuments(enterpriseNumber).then(
-  (documents) => console.log("Documents: ", documents)
-).catch(
-  (err) => console.log(err)
+app.get('/api/v1/documents/:enterpriseNumber', (req, res) => {
+  let enterpriseNumber = String(req.params.enterpriseNumber || "")
+  let normalized = parseInt(enterpriseNumber.replace(/\D/g, ""), 10)
+
+  parser.searchDocuments(normalized).then(
+    (documents) => res.json(documents)
+  ).catch(
+    (err) => res.status(400).json({ err })
+  )
+})
+
+app.listen(app.get('port'), () =>
+  console.log(`BelgianMonitorApi listening on port ${app.get('port')}`)
 )
