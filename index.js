176dcfe5a924e9d3f1ee202b8861929aa8bad4ad
Pierre Paridans <pierre@paridans.org>
2016-04-06 15:04:23 +0200

    added clustered mode (using throng)

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import express from 'express'
 import morgan from 'morgan'
 import parser from './belgian-monitor-parser'
+import throng from 'throng'
 
-let app = express()
-app.set('port', process.env.PORT || 3000)
-app.use(morgan('combined'))
+const WORKERS = process.env.WEB_CONCURRENCY || 1
+const PORT = process.env.PORT || 3000
 
-app.get('/api/v1/documents/:enterpriseNumber', (req, res) => {
-  let enterpriseNumber = parser.normalizeEnterpriseNumber(req.params.enterpriseNumber)
+let start = (workerId) => {
+  let app = express()
+  app.set('port', PORT)
+  app.use(morgan('combined'))
 
-  parser.searchDocuments(enterpriseNumber).then(
-    (documents) => res.json(documents)
-  ).catch(
-    (err) => res.status(400).json({ err })
+  app.get('/api/v1/documents/:enterpriseNumber', (req, res) => {
+    let enterpriseNumber = parser.normalizeEnterpriseNumber(req.params.enterpriseNumber)
+
+    parser.searchDocuments(enterpriseNumber).then(
+      (documents) => res.json(documents)
+    ).catch(
+      (err) => res.status(400).json({ err })
+    )
+  })
+
+  app.listen(app.get('port'), () =>
+    console.log(`BelgianMonitorApi listening [${workerId}] on port ${app.get('port')}`)
   )
-})
+}
 
-app.listen(app.get('port'), () =>
-  console.log(`BelgianMonitorApi listening on port ${app.get('port')}`)
-)
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start)
