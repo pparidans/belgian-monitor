68a81e13449e053a52fe9ff0cf82323c8dc2caca
Pierre Paridans <pierre@paridans.org>
2016-04-07 10:17:44 +0200

    cleanup js related

--- cmd.js
+++
@@ -1,9 +0,0 @@
-import parser from './belgian-monitor-parser'
-
-let enterpriseNumber = parser.normalizeEnterpriseNumber(process.argv[2])
-
-parser.searchDocuments(enterpriseNumber).then(
-  (documents) => console.log(documents)
-).catch(
-  (err) => console.error({ err })
-)
